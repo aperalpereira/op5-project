abefa3603ab4002342d2b9a7ed705b3a63d453fa
Antonio Peral <antonio@alternaenergetica.com>
2020-10-26 11:59:33 +0100

    Added basic test examples

--- test/ejercicio1.test.js
+++ test/ejercicio1.test.js
@@ -2,6 +2,12 @@ const chai = require('chai')
 const ejercicio1 = require('../lib/ejercicio1')
 
 describe('Ejercicio 1',()=>{
+    it('Basic test example',()=>{
+        chai.assert.equal(1,1)
+    })
+    it('Basic test failure example',()=>{
+        chai.assert.equal(1,2)
+    })
     it('Works for synthetic example',()=>{
         
     })
